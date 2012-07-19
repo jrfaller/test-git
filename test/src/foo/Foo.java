c70bc360be1b90e9b243549ba5508bbed811001b
Éloi Rivard (eloi rivard <loi Rivard (eloi.rivard@gmail.com)>
2012-07-19 10:55:25 +0200

    Javadoc comments

--- test/src/foo/Foo.java
+++ test/src/foo/Foo.java
@@ -1,15 +1,24 @@
 package foo;
 
 public class Foo extends Bar {
-	
+
+	/**
+	* The foo constructor.
+	*/	
 	private Foo() {
 		System.out.println("Foo!");
 		id = 0;
 		bar();
 	}
 
-	protected void bar() {
+	/**
+	* The bar method for the win.
+	* @param fu is a dummy value.
+	* @return is useless.
+	*/
+	protected int bar(int fu) {
 		System.out.println("Bar!");
+		return fu - 1;
 	}
 	
 }
